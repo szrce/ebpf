f0808595c7cfdc741b5a217f5dc0f45a6bf4686c
B. Ugur Demirkan <8403350+f0xmulder@users.noreply.github.com>
2025-03-06 01:27:17 +0300

    Update whitelist.py

--- bcc/whitelist.py
+++ bcc/whitelist.py
@@ -42,3 +42,17 @@ b.attach_kprobe(event=syscall, fn_name="block_kill")
 b.trace_print()
 
 #sudo python whitelist.py
+#Linux ubuntuguest 6.11.0-18-generic #18-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  7 22:34:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux
+#PRETTY_NAME="Ubuntu 24.10"
+#NAME="Ubuntu"
+#VERSION_ID="24.10"
+#VERSION="24.10 (Oracular Oriole)"
+#VERSION_CODENAME=oracular
+#ID=ubuntu
+#ID_LIKE=debian
+#HOME_URL="https://www.ubuntu.com/"
+#SUPPORT_URL="https://help.ubuntu.com/"
+#BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
+#PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
+#UBUNTU_CODENAME=oracular
+#LOGO=ubuntu-logo
